54e98318e729a1d45a237d1f4284d0c93fe71641
Sulthon Maulana Fathuddin <sulthonmf@gmail.com>
2021-08-26 13:26:19 +0700

    Menambahakn router

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+//Panggil routes
+var routes = require('./router');
+routes(app);
+
 app.listen(3001, () => {
     console.log(`Server started on port`);
 });
